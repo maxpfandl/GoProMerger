bdeae113cf0b35b8fdc21c5f0fcdc4aa49503c12
Max Pfandl <max@pfandl.name>
2025-12-01 18:41:37 +0100

    update gpxmerge

--- Program.cs
+++ Program.cs
@@ -14,7 +14,6 @@ namespace GoProMerger
         static bool _delete = false;
         static bool _reencode = false;
 
-        static bool _mergegpx = false;
 
         static void Main(string[] args)
         {
@@ -37,10 +36,6 @@ namespace GoProMerger
                         if (arg == "delete")
                         {
                             _delete = true;
-                        }
-                        else if (arg == "mergegpx")
-                        {
-                            _mergegpx = true;
                         }
                         else if (arg == "reencode")
                         {
@@ -67,11 +62,7 @@ namespace GoProMerger
                         }
 
                     }
-                    if ((_reencode && _mergegpx))
-                    {
-                        Console.WriteLine("GoProMerger [delete] [nogroup] [directory] ([reencode] [mixedmerge]|[mergegpx])");
-                        return;
-                    }
+
                 }
 
                 if (files.Length == 0)
@@ -244,18 +235,8 @@ namespace GoProMerger
 
                     ProcessStartInfo psi = new ProcessStartInfo();
                     psi.FileName = _ffmpegPath;
-                    if (_mergegpx)
-                    {
-                         Console.WriteLine("Merging with GPS");
-                        psi.Arguments = $"-hide_banner -loglevel error -f concat -safe 0 -i \"{inputFiles}\" -map 0 -c copy -copy_unknown -tag:d:2 gpmd \"{outputFile}\"";
-                    }
-
-                    else
-                    {
-                        // ffmpeg -y -f concat -safe 0 -i test.txt -c copy -copy_unknown -map 0:v -map 0:a -map 0:2 -map 0:3 -map 0:4 -tag:2 tmcd -tag:3 gpmd -tag:4 fdsc test2.mp4
-                        Console.WriteLine("Merging without GPS");
-                        psi.Arguments = $"-hide_banner -loglevel error -y -f concat -safe 0 -i \"{inputFiles}\" -c copy -map 0:v -map 0:a  \"{outputFile}\"";
-                    }
+                    Console.WriteLine("Merging...");
+                    psi.Arguments = $"-hide_banner -loglevel error -f concat -safe 0 -i \"{inputFiles}\" -map 0 -c copy -ignore_unknown \"{outputFile}\"";
 
                     Process proc = new Process
                     {
